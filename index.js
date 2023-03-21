dd00ff481db733ca1077016245a4458a7f67d6c1
dbfl0 <85721305+dbfl0@users.noreply.github.com>
2023-03-21 15:47:35 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 const express = require('express');
 const app = express();
-const { pool } = require('./db');
+let count = 0;
 
-app.get('/', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT count FROM counter');
-    const count = result.rows[0].count;
-    await pool.query('UPDATE counter SET count = $1', [count + 1]);
-    res.send(`Count: ${count}`);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
+app.get('/', (req, res) => {
+  count++;
+  res.send(`Count: ${count}`);
 });
 
-app.get('/reset', async (req, res) => {
-  try {
-    await pool.query('UPDATE counter SET count = 0');
-    res.send(`Counter has been reset`);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
+app.get('/reset', (req, res) => {
+  count = 0;
+  res.send(`Counter has been reset`);
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
